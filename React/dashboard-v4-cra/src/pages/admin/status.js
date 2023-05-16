6c83999a0b673954fd44c2604dffd836e1f63afd
ImYoo <vincesteffens@gmail.com>
2023-05-16 15:02:50 -0700

    Created divs containing SVGs

---
+++ React/dashboard-v4-cra/src/pages/admin/status.js
@@ -0,0 +1,5 @@
+import Content from '../../components/abundanceVolcanoContent';
+
+export default function StatusPage() {
+  return <Content title="Abundance Volcano" />;
+}
---
+++ React/minimal-reactive-hello-d3/src/views/MultilineChart/MultilineChart.js
@@ -0,0 +1,91 @@
+/** MultilineChart.js */
+import React from "react";
+import * as d3 from "d3";
+
+const MultilineChart = ({ data = [], dimensions = {} }) => {
+  // Note here the definition of svgRef
+  // It looks like this is step 1 of 3
+  const svgRef = React.useRef(null);
+  const { width, height, margin = {} } = dimensions;
+  const svgWidth = width + margin.left + margin.right;
+  const svgHeight = height + margin.top + margin.bottom;
+
+  React.useEffect(() => {
+
+    const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(data[0].items, (d) => d.date))
+      .range([0, width]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([
+        d3.min(data[0].items, (d) => d.value) - 50,
+        d3.max(data[0].items, (d) => d.value) + 50
+      ])
+      .range([height, 0]);
+
+    // Create root container where we will append all other chart elements
+    // Step 2 of 3
+    const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove(); // Clear svg content before adding new elements
+
+    const svg = svgEl
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add X grid lines with labels
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(5)
+      .tickSize(-height + margin.bottom);
+
+    const xAxisGroup = svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(xAxis);
+    xAxisGroup.select(".domain").remove();
+    xAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
+    xAxisGroup
+      .selectAll("text")
+      .attr("opacity", 0.5)
+      .attr("color", "white")
+      .attr("font-size", "0.75rem");
+
+    // Add Y grid lines with labels
+    const yAxis = d3
+      .axisLeft(yScale)
+      .ticks(5)
+      .tickSize(-width)
+      .tickFormat((val) => `${val}%`);
+    const yAxisGroup = svg.append("g").call(yAxis);
+    yAxisGroup.select(".domain").remove();
+    yAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
+    yAxisGroup
+      .selectAll("text")
+      .attr("opacity", 0.5)
+      .attr("color", "white")
+      .attr("font-size", "0.75rem");
+
+    // Draw the lines
+    const line = d3
+      .line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScale(d.value));
+
+    svg
+      .selectAll(".line")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("fill", "none")
+      .attr("stroke", (d) => d.color)
+      .attr("stroke-width", 3)
+      .attr("d", (d) => line(d.items));
+  }, [data]);
+
+  // Step 3 of 3 
+  return <svg ref={svgRef} width={svgWidth} height={svgHeight} />;
+};
+
+export default MultilineChart;
