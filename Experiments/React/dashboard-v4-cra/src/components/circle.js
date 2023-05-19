d1ccdf2fed37ba0a3e682546ec594a97c7a87806
ImYoo <vincesteffens@gmail.com>
2023-05-18 18:49:52 -0700

    Passing parameters

--- Experiments/React/dashboard-v4-cra/src/components/circle.js
+++ Experiments/React/dashboard-v4-cra/src/components/circle.js
@@ -2,11 +2,16 @@ import React from "react"
 
 import * as d3v4 from './d3.v4.js';
 
-const Circle = () => {
-    console.log('foobar')
-    console.log(Object.keys(d3v4))
-    console.log('selectAll' in d3v4)
+const Circle = ({cx, cy}) => {
+    console.log(cx)
+    console.log(cy)
+
     const svgRef = React.useRef(null)
+
+    const measured_width = d3v4.select('.the_circle').node().getBoundingClientRect().width
+    const measured_height = d3v4.select('.the_circle').node().getBoundingClientRect().height;
+    console.log(measured_height)
+    console.log(measured_width)
 
     const margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
@@ -16,23 +21,12 @@ const Circle = () => {
         const svgEl = d3v4.select(svgRef.current)
         svgEl.selectAll("*").remove()
 
-        //const svg = svgEl
-        //    .append('g')
-        //    .attr('transform', `translate(${margin.left},${margin.right})`)
-
-        svgEl
-            .append('svg')
-            .attr('width', margin.left + width  + margin.right)
-            .attr('height', margin.top + height + margin.bottom)
-            .append('g')
-                .attr('transform',
-                      'translate(' + margin.left + ',' + margin.top + ')')
 
         svgEl
             .append('circle')
-            .attr('cx', 20)
-            .attr('cy', 20)
-            .attr('r', 10)
+            .attr('cx', measured_width / 2)
+            .attr('cy', measured_height / 2)
+            .attr('r', 40)
             .style('fill', 'red')
             .style('stroke-width', "3")
             .style('stroke', 'black')
@@ -42,7 +36,6 @@ const Circle = () => {
     return (
       <div>
         <svg ref={svgRef} width={200} height={200}>
-            {/* The circle will be drawn within this SVG */}
         </svg>
       </div>
         );
