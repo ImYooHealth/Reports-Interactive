a2a8fd13548a0cf7977607acfe918015cf632233
ImYoo <vincesteffens@gmail.com>
2023-05-23 16:09:17 -0700

    Preparing to work on Abundance

--- Experiments/React/dashboard-v4-cra/src/components/abundance.js
+++ Experiments/React/dashboard-v4-cra/src/components/abundance.js
@@ -1,48 +1,8 @@
-import * as utils from './utils.js'
+import * as abundance from './abundance_main.js'
 
 const Abundance = () => {
     const svgRef = React.useRef(null)
     const real_data = true
-/*
-var GROUPING
-var ABUNDANCE_VERTICAL
-var SELF
-var data
-
-if (real_data) {
-    GROUPING = 'cell_type' // For demo data use: 'Species'
-    ABUNDANCE_VERTICAL = 'abundance_value'  // For demo data use: 'Sepal_Width'
-    SELF = 'is_self'
-    data = readCSVFile("304/Abundances/COMT.csv")
-    console.log(data)
-    
-    var data_groups = {
-      "others": {
-        "data": [],
-        "point_tag": "other_abundance_points",
-        "point_class": "other_abundance_point",
-        "color": "#C0CCDC",
-        "radius": 3
-      },
-      "self": {
-        "data": [],
-        "point_tag": "self_abundance_points",
-        "point_class": "self_abundance_point",
-        "color": "#D73737",
-        "radius": 7
-      }
-    };
-} else {
-    GROUPING = 'Species'
-    ABUNDANCE_VERTICAL = 'Sepal_Width'
-    SELF = 'is_self'
-    data = readCSVFile("data1.csv")
-}
-*/
-    // Constants
-    var margin = {top: 10, right: 30, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;    
 
     React.useEffect(() => {
         const svgEl = d3v4.select(svgRef.current)
