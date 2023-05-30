6d8430d992db8a754a560d11bea5b895618a58fa
ImYoo <vincesteffens@gmail.com>
2023-05-30 16:12:39 -0700

    Code cleanup

--- Experiments/React/dashboard-v4-cra/src/components/abundanceVolcanoContent.js
+++ Experiments/React/dashboard-v4-cra/src/components/abundanceVolcanoContent.js
@@ -3,31 +3,24 @@ import Abundance from './abundance.js'
 import MyComponent from './demo_component.js'
 
 const Content = ({ title }) => (
-    <div className="lg:w-8/12 bg-gray-800 py-6 px-6 rounded-3xl min-w-min max-w-min">
+  <div className="lg:w-8/12 bg-gray-800 py-6 px-6 rounded-3xl min-w-min max-w-min">
 
-      {/* Just Text */}
-      <div className="flex justify-between text-white items-center mb-8">
-        <p className="text-2xl font-bold">{title}</p>
-        <p className="">December, 12</p>
-      </div>
+    {/* Just Text */}
+    <div className="flex justify-between text-white items-center mb-8">
+      <p className="text-2xl font-bold">{title}</p>
+    </div>
 
-      <div className="flex flex-wrap">
+    <div className="flex flex-wrap">
+      <div className="p-4 rounded-3xl bg-gray-300">
+        {/* Abundance */}
+        <div className="p-7 bg-white rounded-3xl">
+          <Abundance />
 
-          {/* Abundance */}
-
-            <div className="p-4 rounded-3xl bg-gray-300">
-              {/* Abundance */}
-              <div className="p-7 bg-white rounded-3xl">
-
-                <Abundance />
-
-              </div>
-            </div>
-
-
+        </div>
       </div>
 
     </div>
+  </div>
 );
 
 export default Content;
