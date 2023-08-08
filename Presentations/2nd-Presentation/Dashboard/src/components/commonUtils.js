fdba5f62a67634b93ec039829ad63d77b0d57a6f
ImYoo <vincesteffens@gmail.com>
2023-08-08 12:58:31 -0700

    Abstract out overhead

--- Presentations/2nd-Presentation/Dashboard/src/components/commonUtils.js
+++ Presentations/2nd-Presentation/Dashboard/src/components/commonUtils.js
@@ -1,3 +1,27 @@
 var deployed = true;
 
 export const feedbackPath = deployed ? 'https://samplereportfeedback.imyoo.health/' : 'http://localhost:31338/'
+
+export const Overhead = ({ children, name }) => {
+    return (
+        <div className="lg:w-8/12 bg-gray-800 py-6 px-6 rounded-3xl min-w-min max-w-min">
+
+            {/* Just Text */}
+            <div className="flex justify-between text-white items-center mb-8">
+                <p className="text-2xl font-bold" style={{fontFamily: "Space Grotesk"}}>
+                    {name}
+                </p>
+            </div>
+
+            <div className="flex flex-wrap">
+                <div className="p-4 rounded-3xl bg-gray-300">
+
+                    <div className="p-7 bg-white rounded-3xl">
+                        {children}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
