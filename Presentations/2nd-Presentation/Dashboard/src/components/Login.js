d811a1c27e0eeba13698f6885a86870cb2fb73b7
ImYoo <vincesteffens@gmail.com>
2023-08-08 12:58:31 -0700

    Abstract out overhead

---
+++ Presentations/2nd-Presentation/Dashboard/src/components/Login.js
@@ -0,0 +1,14 @@
+import * as commonUtils from './commonUtils.js'
+import ExampleLogin from './exampleLogin.js'
+import CommentBox from './comment_box'
+
+const Login = () => {
+    return (
+        <commonUtils.Overhead>
+            <ExampleLogin name="Login"/>
+        </commonUtils.Overhead>
+    );
+}
+
+export default Login
+
