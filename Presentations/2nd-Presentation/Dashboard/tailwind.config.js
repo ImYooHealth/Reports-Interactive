74eb20b7ea7c0d1c0626bdf0c95c698907563b17
ImYoo <vincesteffens@gmail.com>
2023-08-17 15:23:39 -0700

    Figured out the width issue

--- Presentations/2nd-Presentation/Dashboard/tailwind.config.js
+++ Presentations/2nd-Presentation/Dashboard/tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
+  important: true,
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/sections/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],    
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '50': '50%',
+        'disclaimer': '850px',
+      },        
+    },
   },
   variants: {
     extend: {},
