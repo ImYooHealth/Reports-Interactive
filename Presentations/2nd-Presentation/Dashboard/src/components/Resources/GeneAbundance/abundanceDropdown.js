135d1eadf6f0e7418f069d03e8743d892eac2ac8
ImYoo <vincesteffens@gmail.com>
2023-09-14 20:14:18 -0700

    Cleanup

--- Presentations/2nd-Presentation/Dashboard/src/components/Resources/GeneAbundance/abundanceDropdown.js
+++ Presentations/2nd-Presentation/Dashboard/src/components/Resources/GeneAbundance/abundanceDropdown.js
@@ -6,42 +6,17 @@ import * as AbundanceUtils from './abundance-utils.js'
 function AbundanceDropdown({options, setSelectedOption, selectedOption}) {
     console.log(options)
     var initialValue = {value: selectedOption, label: selectedOption}
-  const [displayedValue, setDisplayedValue] = React.useState(initialValue);
-/*
-    console.log(options[0])
-        console.log('-----------')
-  console.log(selectedOption)
-      console.log('-----------')
-*/
-  // Dev
-  const [inputText, setInputText] = React.useState('');
-  const [sendText, setSendText] = React.useState("Send!")
+    const [displayedValue, setDisplayedValue] = React.useState(initialValue);
 
-  const handleClickSend = () => {
-    setSendText(sendText === "Send!" ? "Sent!" : "Send!")
-    setSelectedOption(options[5])
-        setDisplayedValue(options[5])
-    setInputText('')
-  }
 
-  const handleSelectChange = (option) => {
-    setSelectedOption(option.value);
-        console.log('-----------')
-    console.log(displayedValue)
-    setDisplayedValue(option)
-    console.log(displayedValue)
-        console.log('-----------')
-  }
+    const handleSelectChange = (option) => {
+        setSelectedOption(option.value);
+        setDisplayedValue(option)
+    }
 
-  const handleInputChange = (event) => {
-    setSendText(sendText === "Sent!" ? "Send!" : "Send!")
-    setInputText(event.target.value)
-  }
-
-  React.useEffect(() => {
-    console.log('Parent state changed')
-    setDisplayedValue({value: selectedOption, label: selectedOption})
-  }, [selectedOption]);
+    React.useEffect(() => {
+        setDisplayedValue({value: selectedOption, label: selectedOption})
+    }, [selectedOption]);
   
   return (
         <Select 
